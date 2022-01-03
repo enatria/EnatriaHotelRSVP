fc6ca4b4dca91a0a5058b464101f089e12077fe2
gidwikintan <gidwikintan@gmail.com>
2022-01-03 15:05:06 +0700

    CONFIG GET HOTELS

--- src/utils/apiService/index.js
+++ src/utils/apiService/index.js
@@ -26,3 +26,32 @@ export const getDestinationId = (query = query) => {
       return error;
     });
 };
+
+export const getHotels = params => {
+  const options = {
+    method: 'GET',
+    url: URL + '/hotels/search',
+    params: {
+      checkin_date: params.checkin_date,
+      checkout_date: params.checkout_date,
+      sort_order: 'STAR_RATING_HIGHEST_FIRST',
+      destination_id: params.destination_id,
+      adults_number: params.adults_number,
+      locale: 'en_US',
+      currency: 'USD',
+      guest_rating_min: '4',
+    },
+    headers: HEADERS,
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      return response;
+    })
+    .catch(function (error) {
+      console.error(error);
+      return error;
+    });
+};
